ef6c0914b3b36766a208d395e1e5345fcdb8f8ce
pariwesh <tamrpariwesh@gmail.com>
2023-11-28 11:07:25 +1100

    initial commit, landing page ui done

---
+++ app/(landing)/layout.tsx
@@ -0,0 +1,15 @@
+import { Footer } from "./_components/footer"
+import { Navbar } from "./_components/navbar"
+
+const LandingLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full bg-slate-100">
+      {/* Navbar */}
+      <Navbar />
+      <main className="pt-40 pb-20 bg-slate-100">{children}</main>
+      {/* Footer */}
+      <Footer />
+    </div>
+  )
+}
+export default LandingLayout
