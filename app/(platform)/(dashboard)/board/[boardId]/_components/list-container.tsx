1dfa2656180cd4cc6bb4a7aa66084ace2b6a33d3
pariwesh <tamrpariwesh@gmail.com>
2023-12-10 13:00:42 +1100

    added feature to add list and rename lists within a board page

---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import { ListWithCards } from "@/types"
+import { ListForm } from "./list-form"
+import { useEffect, useState } from "react"
+import { ListItem } from "./list-item"
+
+type ListContainerProps = {
+  data: ListWithCards[]
+  boardId: string
+}
+
+export const ListContainer = ({ data, boardId }: ListContainerProps) => {
+  const [orderedData, setOrderedData] = useState(data)
+
+  useEffect(() => {
+    setOrderedData(data)
+  }, [data])
+  return (
+    <ol className="flex gap-x-3 h-full">
+      {orderedData.map((list, i) => (
+        <ListItem key={list.id} index={i} data={list} />
+      ))}
+      <ListForm />
+      <div className="flex-shrink-0 w-1" />
+    </ol>
+  )
+}
